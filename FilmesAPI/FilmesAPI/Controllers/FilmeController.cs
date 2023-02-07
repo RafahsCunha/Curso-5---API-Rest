c227b51a44338db199d6b7ded1b4a73bc7e97a40
RafahsCunha <rafaelhsc20@gmail.com>
2023-02-06 22:53:25 -0300

    Primeiro commit

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)//[FromBody] a informação do filme vem do corpo da requisição, ou seja, do post que o usuario fez
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        } 
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "O campo Título é obrigatório")]// Indica que o Titulo é um campo obrigatório. ErrorMessage define qual será a mensagem de erro exibida
+        public string Titulo { get; set; }
+
+        [Required (ErrorMessage = "Campo Diretor é obrigatório")]// Indica que o Diretor é um campo obrigatório
+        public string Diretor { get; set; }
+
+        [StringLength(30,ErrorMessage = "Genero nao pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1, 600,ErrorMessage="Deve estar entre 1 e 600")]// Indica que filme pode durar no minimo 1 e no máximo 600
+        public int Duracao { get; set; }
+    }
+}
