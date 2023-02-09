8b3d32067b6ed118536e6348f4d78374aef84b54
RafahsCunha <rafaelhsc20@gmail.com>
2023-02-08 22:13:10 -0300

    Metodo httpget com parametro pela URL

--- FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -18,9 +18,22 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperarFilmes()
+        public IEnumerable<Filme> RecuperarFilmes() // IEnumerable é uma interface que herdada na classe list 
         {
             return filmes;
-        } 
+        }
+
+        [HttpGet("{id}")]// a expressão "{id}" é a forma de passar o parâmetro id do método abaixo. O valor é passado dentro da URL ex.: https://localhost:7206/filme/1 . A url com /1 é a forma de saber que estamos utilizando o httpget com parâmetro
+        public Filme RecuperaFilmePeloId(int id)
+        {
+            foreach(Filme filme in filmes)
+            {
+                if(filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+            return null;
+        }
     }
 }
